f39eda72f56fdd89e884849080c7bbd38112fd59
Lukiiy <102190774+Lukiiy@users.noreply.github.com>
2026-05-30 15:17:01 -0300

    Updated main.go
    * Added mode checker (uh, modern, transitive and legacy)
    * Added the file write call
    * Replaced SupportedFormats type to []int
    * Coded the actual functionality of replacing the original mcmeta
      version spec with a given one

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
     "encoding/json"
     "fmt"
     "os"
+    "strconv"
+    "slices"
 )
 
 type Meta struct {
@@ -13,14 +15,9 @@ type Meta struct {
 type Pack struct {
     Description any `json:"description"`
     PackFormat *int `json:"pack_format,omitempty"`
-    SupportedFormats *SupportedFormats `json:"supported_formats,omitempty"`
+    SupportedFormats *[]int `json:"supported_formats,omitempty"`
     MinFormat *int `json:"min_format,omitempty"`
     MaxFormat *int `json:"max_format,omitempty"`
-}
-
-type SupportedFormats struct {
-    Min int `json:"min_inclusive"`
-    Max int `json:"max_inclusive"`
 }
 
 func main() {
@@ -31,6 +28,12 @@ func main() {
 
     file := os.Args[1]
 
+    fileInfo, err := os.Stat(file)
+	if err != nil {
+		panic(err)
+	}
+	fileMode := fileInfo.Mode()
+
     stuff, err := os.ReadFile(file)
     if err != nil {
         panic(err)
@@ -40,4 +43,68 @@ func main() {
     if err := json.Unmarshal(stuff, &meta); err != nil {
         panic(err)
     }
+
+    if meta.Pack == nil {
+        return
+    }
+
+    mainFormat, err := strconv.Atoi(os.Args[2])
+    if err != nil {
+	    fmt.Println("Main format must be an integer")
+	    return
+    }
+
+    allFormats := []int{mainFormat}
+
+    for _, arg := range os.Args[3:] {
+        val, err := strconv.Atoi(arg)
+        if err != nil {
+            fmt.Println("Compatible formats must be all integers")
+            return
+        }
+
+        allFormats = append(allFormats, val)
+    }
+
+    mode := detect(meta)
+
+    meta.Pack.PackFormat = &mainFormat
+    meta.Pack.SupportedFormats = nil
+    meta.Pack.MinFormat = nil
+    meta.Pack.MaxFormat = nil
+
+    min := slices.Min(allFormats)
+    max := slices.Max(allFormats)
+
+    switch mode {
+	    case "modern":
+	        meta.Pack.MinFormat = &min
+	        meta.Pack.MaxFormat = &max
+	    case "transitional":
+     		meta.Pack.SupportedFormats = &[]int{min, max}
+    }
+
+    out, err := json.MarshalIndent(meta, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(file, out, fileMode)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Updated! Mode:", mode)
+}
+
+func detect(meta Meta) string {
+    if meta.Pack.MinFormat != nil || meta.Pack.MaxFormat != nil {
+        return "modern"
+    }
+
+    if meta.Pack.SupportedFormats != nil {
+        return "transitional"
+    }
+
+    return "legacy"
 }
